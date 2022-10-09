ba43329afc16dbfef27a6ff661f80fb8a8daa078
ed <wantuiliv@gmail.com>
2022-10-09 20:00:26 -0300

    test: register valid user to mailing list

---
+++ test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,18 @@
+import { UserData } from '../../../src/entities/user-data'
+import { UserRepository } from '../../../src/usecases/register-user-on-mailing-list/ports/user-repository'
+import { RegisterUserOnMailingList } from '../../../src/usecases/register-user-on-mailing-list/register-user-on-mailing-list'
+import { InMemoryUserRepository } from '../../../src/usecases/register-user-on-mailing-list/repository/in-memory-repository'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const response = await useCase.registerUserOnMaillingList({ name, email })
+    const user = repo.findUserByEmail('any@email.com')
+    expect((await user).name).toBe('any_name')
+    expect(response.value.name).toEqual('any_name')
+  })
+})
