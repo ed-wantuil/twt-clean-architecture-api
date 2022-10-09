83f3251608ad60c4ac5b15531a87d4d711c3abf3
ed <wantuiliv@gmail.com>
2022-10-09 18:16:49 -0300

    test: verify name while creating user

--- test/entities/user.spec.ts
+++ test/entities/user.spec.ts
@@ -8,4 +8,16 @@ describe('User domain class', () => {
     const error = User.create({ name: 'any_name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
   })
+
+  test('should not create user with invalid name (too few characters)', () => {
+    const invalidName = 'O      '
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid name (too many characters)', () => {
+    const invalidName = 'O'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidEmailError()))
+  })
 })
