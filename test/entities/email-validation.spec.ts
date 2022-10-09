622873ad2b07e38ca8942709ba0b4ffb9fdac9e4
ed <wantuiliv@gmail.com>
2022-10-09 15:48:21 -0300

    test: validate emails with more than 320 chars

--- test/entities/email-validation.spec.ts
+++ test/entities/email-validation.spec.ts
@@ -17,6 +17,11 @@ describe('Email validation', () => {
   })
 
   test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
