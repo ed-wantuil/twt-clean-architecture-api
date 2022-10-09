4265cd3c91a5b2f9318f50c19683070c684afc4e
ed <wantuiliv@gmail.com>
2022-10-09 15:53:41 -0300

    test: validate emails with domain larger than 255 chars

--- test/entities/email-validation.spec.ts
+++ test/entities/email-validation.spec.ts
@@ -16,8 +16,13 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeTruthy()
   })
 
-  test('should not accept local part larger than 64 chars', () => {
+  test('should not accept strings larger than 320 chars', () => {
     const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept domain part larger than 255 chars', () => {
+    const email = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
     expect(Email.validate(email)).toBeFalsy()
   })
 
