43b18176517e58b37a4bc66276c296c35596a701
ed <wantuiliv@gmail.com>
2022-10-10 05:32:49 -0300

    refactor: use path mapping to improve imports

--- test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
-import { InMemoryUserRepository } from '../../../../src/usecases/register-user-on-mailing-list/repository'
-import { UserData } from '../../../../src/entities'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
+import { UserData } from '@/entities'
 
 describe('In memory User repository', () => {
   test('should return null if user is not  found', async () => {
