85c18397ee743837accd1d3f60d46317d306d080
ed <wantuiliv@gmail.com>
2022-10-11 17:15:42 -0300

    feat: validate the request fields

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,7 +1,8 @@
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
 import { UserData } from '@/entities'
-import { create } from '@/web-controllers/util'
+import { badRequest, create } from '@/web-controllers/util'
+import { MissingParamError } from '@/web-controllers/erros/missing-param-error'
 
 export class RegisterUserController {
   private readonly useCase: RegisterUserOnMailingList
@@ -11,8 +12,23 @@ export class RegisterUserController {
   }
 
   public async handle (request: HttpRequest): Promise<HttpResponse> {
+    if (!request.body.name || !request.body.email) {
+      let missingParam: string = !request.body.name ? 'name ' : ''
+      missingParam += !request.body.email ? 'email' : ''
+
+      return badRequest(new MissingParamError(missingParam.trim()))
+    }
+
+    if (!request.body.email) {
+      return badRequest('email')
+    }
+
     const userData: UserData = request.body
     const response = await this.useCase.registerUserOnMaillingList(userData)
+
+    if (response.isLeft()) {
+      return badRequest(response.value)
+    }
 
     if (response.isRight()) {
       return create(response.value)
