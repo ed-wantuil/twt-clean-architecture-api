ba0d4948652e6a99db82c5db9d1fbcadb2e290dd
ed <wantuiliv@gmail.com>
2022-10-11 17:38:23 -0300

    feat: web controller should return code 500 when server raises

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -1,16 +1,16 @@
-import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { UserRepository, UseCase } from '@/usecases/register-user-on-mailing-list/ports'
 import { UserData, User } from '@/entities'
 import { Either, left, right } from '@/shared'
 import { InvalidNameError, InvalidEmailError } from '@/entities/errors'
 
-export class RegisterUserOnMailingList {
+export class RegisterUserOnMailingList implements UseCase {
   private readonly userRepo: UserRepository
 
   constructor (userRepo: UserRepository) {
     this.userRepo = userRepo
   }
 
-  async registerUserOnMaillingList (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+  async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
     const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
     if (userOrError.isLeft()) {
       return left(userOrError.value)
--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,37 +1,41 @@
-import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
 import { UserData } from '@/entities'
-import { badRequest, create } from '@/web-controllers/util'
+import { badRequest, create, serverError } from '@/web-controllers/util'
 import { MissingParamError } from '@/web-controllers/erros/missing-param-error'
+import { UseCase } from '@/usecases/register-user-on-mailing-list/ports'
 
 export class RegisterUserController {
-  private readonly useCase: RegisterUserOnMailingList
+  private readonly useCase: UseCase
 
-  constructor (useCase: RegisterUserOnMailingList) {
+  constructor (useCase: UseCase) {
     this.useCase = useCase
   }
 
   public async handle (request: HttpRequest): Promise<HttpResponse> {
-    if (!request.body.name || !request.body.email) {
-      let missingParam: string = !request.body.name ? 'name ' : ''
-      missingParam += !request.body.email ? 'email' : ''
+    try {
+      if (!request.body.name || !request.body.email) {
+        let missingParam: string = !request.body.name ? 'name ' : ''
+        missingParam += !request.body.email ? 'email' : ''
 
-      return badRequest(new MissingParamError(missingParam.trim()))
-    }
+        return badRequest(new MissingParamError(missingParam.trim()))
+      }
 
-    if (!request.body.email) {
-      return badRequest('email')
-    }
+      if (!request.body.email) {
+        return badRequest('email')
+      }
 
-    const userData: UserData = request.body
-    const response = await this.useCase.registerUserOnMaillingList(userData)
+      const userData: UserData = request.body
+      const response = await this.useCase.perform(userData)
 
-    if (response.isLeft()) {
-      return badRequest(response.value)
-    }
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
 
-    if (response.isRight()) {
-      return create(response.value)
+      if (response.isRight()) {
+        return create(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
