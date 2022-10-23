51236840f37840e0c7b77fa0189d30db2889e295
Ed <wantuiliv@gmail.com>
2022-10-23 20:14:33 -0300

    feat: register user controller factor

---
+++ src/main/factories/registrer.ts
@@ -0,0 +1,9 @@
+import { RegisterUserController } from '@/web-controllers'
+import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/register-user-on-mailing-list'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
+
+export const makeRegisterUserController = (): RegisterUserController => {
+  const inMemoryUserRepository = new InMemoryUserRepository([])
+  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(inMemoryUserRepository)
+  return new RegisterUserController(registerUserOnMailingListUseCase)
+}
