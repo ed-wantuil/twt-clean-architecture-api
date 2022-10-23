cd6e3ea49e437090666242499ab3500880ae5cd3
Ed <wantuiliv@gmail.com>
2022-10-23 20:47:40 -0300

    test: config register route

---
+++ src/main/config/routes.ts
@@ -0,0 +1,10 @@
+import { readdirSync } from 'fs'
+import { Express, Router } from 'express'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  readdirSync(`${__dirname}/routes`).map(async file => {
+    (await import(`${__dirname}/routes/${file}`)).default(router)
+  })
+}
