c81607c760178e21c4226254094cb85736e0d3ea
Edmar Wantuil <wantuiliv@gmail.com>
2023-01-07 08:53:38 -0300

    chore: configure jest mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
+  present: '@shelf/jest-mongodb',
   roots: ['<rootDir>/test'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
